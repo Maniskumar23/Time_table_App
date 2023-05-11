1d2635b486e79241353aaf0bf240c78a71244714
Manish Kumar <maniskvs38@gmail.com>
2023-05-12 01:03:13 +0530

    MK commit

--- MY_Timetable/app/src/main/java/com/example/my_timetable/MainActivity.java
+++ MY_Timetable/app/src/main/java/com/example/my_timetable/MainActivity.java
@@ -2,13 +2,80 @@ package com.example.my_timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton monImageButton;
+    ImageButton tueImageButton;
+    ImageButton wedImageButton;
+    ImageButton thuImageButton;
+    ImageButton friImageButton;
+    ImageButton satImageButton;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.timetable_page);
+        setContentView(R.layout.activity_main);
+
+        monImageButton=(ImageButton)findViewById(R.id.mon);
+        tueImageButton=(ImageButton)findViewById(R.id.tue);
+        wedImageButton=(ImageButton)findViewById(R.id.wed);
+        thuImageButton=(ImageButton)findViewById(R.id.thu);
+        friImageButton=(ImageButton)findViewById(R.id.fri);
+        satImageButton=(ImageButton)findViewById(R.id.sat);
+
+        monImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity.this, MainActivity_mon.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+        tueImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity.this, MainActivitytue.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        wedImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity.this, MainActivity_wed.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        thuImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity.this, MainActivity_thu.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        friImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity.this, MainActivity_fri.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        satImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity.this, MainActivity_sat.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
 }
---
+++ MY_Timetable/app/src/main/java/com/example/my_timetable/MainActivity_thu.java
@@ -0,0 +1,14 @@
+package com.example.my_timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_thu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_thu);
+    }
+}
