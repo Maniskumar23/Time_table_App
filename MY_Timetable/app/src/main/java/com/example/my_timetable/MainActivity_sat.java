b5b003a3ffea1f93b732427d03493403d6f7e830
nitesh pandey <niteshpandey46974@gmail.com>
2023-05-17 00:22:52 +0530

    pandey commit

--- MY_Timetable/app/src/main/java/com/example/my_timetable/MainActivity_sat.java
+++ MY_Timetable/app/src/main/java/com/example/my_timetable/MainActivity_sat.java
@@ -2,13 +2,47 @@ package com.example.my_timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity_sat extends AppCompatActivity {
+
+    ImageButton monImageButton;
+    ImageButton tueImageButton;
+    ImageButton wedImageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_sat);
+
+        monImageButton=(ImageButton)findViewById(R.id.bx);
+        tueImageButton=(ImageButton)findViewById(R.id.by);
+        wedImageButton=(ImageButton)findViewById(R.id.bz);
+
+        monImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity_sat.this, MainActivity_bx_sat.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+        tueImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity_sat.this, MainActivity_by_sat.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+
+        wedImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity_sat.this, MainActivity_bz_sat.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
 }
