b6059409ef3eaaa52851c82df8ee3321935aa240
manish kumar <maniskvs38@gmail.com>
2023-05-17 00:31:01 +0530

    mk commit

---
+++ MY_Timetable/app/src/main/java/com/example/my_timetable/MainActivity_first_page.java
@@ -0,0 +1,14 @@
+package com.example.my_timetable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity_first_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_first_page);
+    }
+}
