1c84ef74a04ed0a584f0a25a26f2272f20d6edad
manish kumar <maniskvs38@gmail.com>
2023-05-17 01:00:04 +0530

    mk commit

--- MY_Timetable/app/src/main/java/com/example/my_timetable/MainActivity_first_page.java
+++ MY_Timetable/app/src/main/java/com/example/my_timetable/MainActivity_first_page.java
@@ -2,13 +2,27 @@ package com.example.my_timetable;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity_first_page extends AppCompatActivity {
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_first_page);
+
+        button=(Button)findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentLoadNewActivity=new Intent(MainActivity_first_page.this, MainActivity.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
     }
 }
